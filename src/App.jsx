694b27c5d66c736bcb0e46803c447d9b56589069
Dr. Ernesto Lee <ernestolee@MacBook-Pro-3.local>
2026-04-08 12:03:45 -0400

    Ensure dummy callback is a valid URL

--- src/App.jsx
+++ src/App.jsx
@@ -124,7 +124,7 @@ function App() {
           model,
           instrumental: !withLyrics,
           customMode: false,
-          callBackUrl: ""
+          callBackUrl: "https://example.com/callback"
         })
       });
 
